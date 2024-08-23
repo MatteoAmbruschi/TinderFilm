4529bb5ee4a92aec1bc855054a149e9212d5ba40
Matteo <matteoambruschi10@gmail.com>
2024-08-23 17:17:37 +0200

    card creata

--- frontend/src/components/myUi/dialogCard/DialogCard.tsx
+++ frontend/src/components/myUi/dialogCard/DialogCard.tsx
@@ -8,10 +8,11 @@ import {
     DialogClose,
     DialogDescription,
     DialogContainer,
-  } from '@/components/core/dialog';
+  } from '@/components/myUi/Dialog';
   import { PlusIcon } from 'lucide-react';
   
-  export function DialogBasicOne() {
+  export function DialogBasicOne({movie}: {movie: any}) {
+    console.log(movie)
     return (
       <Dialog
         transition={{
@@ -20,26 +21,27 @@ import {
           duration: 0.25,
         }}
       >
-        <DialogTrigger
+        <div
           style={{
             borderRadius: '12px',
           }}
-          className='flex max-w-[270px] flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900'
+          className='flex flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900'
         >
           <DialogImage
-            src='/eb-27-lamp-edouard-wilfrid-buquet.jpg'
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
             alt='A desk lamp designed by Edouard Wilfrid Buquet in 1925. It features a double-arm design and is made from nickel-plated brass, aluminium and varnished wood.'
-            className='h-48 w-full object-cover'
+            className='h-[346px] w-full object-cover'
           />
-          <div className='flex flex-grow flex-row items-end justify-between p-2'>
+          <div className='flex flex-grow flex-row items-end justify-between p-2 h-[90px]'>
             <div>
-              <DialogTitle className='text-zinc-950 dark:text-zinc-50'>
-                EB27
+              <DialogTitle className='text-zinc-950 dark:text-zinc-50 leading-4 pb-1'>
+                {movie.title}
               </DialogTitle>
               <DialogSubtitle className='text-zinc-700 dark:text-zinc-400'>
-                Edouard Wilfrid Buquet
+              Rating: {movie.vote_average}
               </DialogSubtitle>
             </div>
+            <DialogTrigger>
             <button
               type='button'
               className='relative ml-1 flex h-6 w-6 shrink-0 scale-100 select-none appearance-none items-center justify-center rounded-lg border border-zinc-950/10 text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-800 focus-visible:ring-2 active:scale-[0.98] dark:border-zinc-50/10 dark:bg-zinc-900 dark:text-zinc-500 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:focus-visible:ring-zinc-500'
@@ -47,8 +49,9 @@ import {
             >
               <PlusIcon size={12} />
             </button>
+            </DialogTrigger>
           </div>
-        </DialogTrigger>
+        </div>
         <DialogContainer>
           <DialogContent
             style={{
@@ -57,16 +60,16 @@ import {
             className='pointer-events-auto relative flex h-auto w-full flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900 sm:w-[500px]'
           >
             <DialogImage
-              src='/eb-27-lamp-edouard-wilfrid-buquet.jpg'
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
               alt='A desk lamp designed by Edouard Wilfrid Buquet in 1925. It features a double-arm design and is made from nickel-plated brass, aluminium and varnished wood.'
               className='h-full w-full'
             />
             <div className='p-6'>
               <DialogTitle className='text-2xl text-zinc-950 dark:text-zinc-50'>
-                EB27
+                {movie.title}
               </DialogTitle>
               <DialogSubtitle className='text-zinc-700 dark:text-zinc-400'>
-                Edouard Wilfrid Buquet
+              Rating: {movie.vote_average}
               </DialogSubtitle>
               <DialogDescription
                 disableLayoutAnimation
@@ -77,23 +80,8 @@ import {
                 }}
               >
                 <p className='mt-2 text-zinc-500 dark:text-zinc-500'>
-                  Little is known about the life of Édouard-Wilfrid Buquet. He was
-                  born in France in 1866, but the time and place of his death is
-                  unfortunately a mystery.
+                    {movie.overview}
                 </p>
-                <p className='text-zinc-500'>
-                  Research conducted in the 1970s revealed that he’d designed the
-                  “EB 27” double-arm desk lamp in 1925, handcrafting it from
-                  nickel-plated brass, aluminium and varnished wood.
-                </p>
-                <a
-                  className='mt-2 inline-flex text-zinc-500 underline'
-                  href='https://www.are.na/block/12759029'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Are.na block
-                </a>
               </DialogDescription>
             </div>
             <DialogClose className='text-zinc-50' />
--- frontend/src/components/tinderFilmCard/TinderFilmCard.jsx
+++ frontend/src/components/tinderFilmCard/TinderFilmCard.jsx
@@ -2,8 +2,7 @@ import React, { useState, useMemo, useRef, useEffect } from 'react'
 import TinderCard from 'react-tinder-card'
 import styles from './tinderFilmCard.module.css'
 import axios from 'axios'
-
-
+import { DialogBasicOne } from '../myUi/dialogCard/DialogCard'
 function Advanced ({className, idApp}) {
 
   const [currentIndex, setCurrentIndex] = useState(-1)
@@ -98,21 +97,24 @@ function Advanced ({className, idApp}) {
       <h1 className={styles.h1}>{info.type ? info.type : 'Loading...'}</h1>
       <div className={styles.cardContainer}>
         {movies.map((movie, index) => (
-          <TinderCard
-            ref={childRefs[index]}
-            className={styles.swipe}
-            key={movie.title}
-            onSwipe={(dir) => swiped(dir, movie.title, index)}
-            onCardLeftScreen={() => outOfFrame(movie.title, index)}
-            preventSwipe={['up', 'down']}
-          >
-            <div
-              style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.poster_path})` }}
-              className={styles.card}
+          <>
+
+            <TinderCard
+              ref={childRefs[index]}
+              className={styles.swipe}
+              key={movie.title}
+              onSwipe={(dir) => swiped(dir, movie.title, index)}
+              onCardLeftScreen={() => outOfFrame(movie.title, index)}
+              preventSwipe={['up', 'down']}
             >
-              <h3>{movie.title}</h3>
-            </div>
-          </TinderCard>
+              <div
+                /* style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.poster_path})` }} */
+                className={styles.card}
+              >
+                <DialogBasicOne movie={movie}></DialogBasicOne>
+              </div>
+            </TinderCard>
+          </>
         ))}
       </div>
           <div className={styles.buttons}>
