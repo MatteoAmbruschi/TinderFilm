92af4856ff7671b1349d227136f54de4036d5892
Matteo <matteoambruschi10@gmail.com>
2024-09-02 10:38:28 +0200

    prova wss

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     const socketInstance = socketIO(
-      process.env.NEXT_PUBLIC_BACKEND || "https://tinder-film-backend.vercel.app",
+      "wss://tinder-film-backend.vercel.app",
       {
         transports: ["websocket"], // Forza il trasporto WebSocket
       }
