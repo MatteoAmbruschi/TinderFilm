5129473ebc8f063dbe4795127e8c74b94fe59a53
Matteo <matteoambruschi10@gmail.com>
2024-09-02 18:11:18 +0200

    zindex

--- frontend/src/components/menu/Menu.tsx
+++ frontend/src/components/menu/Menu.tsx
@@ -2,7 +2,7 @@ import { SegmentedControl } from "../myUi/segmented-control/SegmentedControl";
 
 export function Menu({className, idApp }: {className: string, idApp: any}) {
   return (
-    <div className={`flex flex-col items-center justify-between p-6 sticky top-0 z-999999 ${className}`}>
+    <div className={`flex flex-col items-center justify-between p-6 sticky top-0 z-50 ${className}`}>
       <SegmentedControl items={['LOBBY', 'MOVIE', 'MATCH']} idApp={idApp}></SegmentedControl>
     </div>
   );
