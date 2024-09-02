47f645ae3901df25d5ae7c563c2154d9f15bc4b6
Matteo <matteoambruschi10@gmail.com>
2024-09-02 19:03:33 +0200

    sempre lettura cookie

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -85,7 +85,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
   useEffect(() => {
-    path !=='/invite' ? readCookie(setIdApp, setIdUser, router) : ''
+    /* path !=='/invite' ?  */readCookie(setIdApp, setIdUser, router) /* : '' */
   }, [idApp])
 
   console.log(path)
